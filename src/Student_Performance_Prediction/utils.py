594af7f6821af901b1174bc86e2610effc02badd
ashishpra28 <aknexa@gmail.com>
2026-02-21 23:50:15 +0530

    Data Ingestion

---
+++ src/Student_Performance_Prediction/utils.py
@@ -0,0 +1,32 @@
+import os
+import sys 
+from src.Student_Performance_Prediction.exception import CustomException 
+from src.Student_Performance_Prediction.logger import logging 
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+database = os.getenv("database")
+
+def read_sql_data():
+    logging.info("Reading MySQL database started")
+    try:
+        mydb = pymysql.connect(
+            host = host,
+            user = user,
+            password = password,
+            database = database
+        )
+        logging.info("Connection established",mydb)
+        df = pd.read_sql_query('select * from students',mydb)
+        print(df.head())
+
+        return df 
+    
+    except Exception as e:
+        raise CustomException(e)
